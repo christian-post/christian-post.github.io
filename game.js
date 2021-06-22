fff3c6ae99a4818b406c6611b4630b6696e8d753
Christian Post <73224559+christian-post@users.noreply.github.com>
2021-06-22 20:44:21 +0200

    Add files via upload

--- game.js
+++ game.js
@@ -31,6 +31,10 @@ function preload ()
         'assets/dude.png',
         { frameWidth: 32, frameHeight: 48 }
     );
+	this.load.spritesheet('explosion', 
+        'assets/explosion.png',
+        {frameWidth: 64, frameHeight: 64}
+    );
     this.load.image('particle', 'assets/particle.png');
 }
 
@@ -40,14 +44,17 @@ var cursors;
 var stars;
 var score = 0;
 var scoreText;
-var maxLives = 1;
+var maxLives = 3;
 var lives = maxLives;
 var livesText;
 var bombs;
-const starCount = 1;
+const starCount = 5;
 const starCollectScore = 10;
+var starLifespan = 10000;
 var level = 0;
+
 var particles;
+var starParticles;
 
 
 function create ()
@@ -98,6 +105,7 @@ function create ()
     // -- COLLECTIBLES ---------------------------------------------------------
 
     stars = this.physics.add.group();
+	starParticles = this.add.particles('star');
     createStars();
 
     this.physics.add.collider(stars, platforms);
@@ -117,8 +125,14 @@ function create ()
      
 
     // -- EFFECTS --------------------------------------------------------------
-    particles = this.add.particles('particle');
+    bombParticles = this.add.particles('particle');
     
+	this.anims.create({
+		key: 'bombExplosion',
+		frames: this.anims.generateFrameNumbers('explosion'),
+		frameRate: 30,
+		hideOnComplete: true
+	});
 
 }
 
@@ -147,11 +161,29 @@ function update ()
     {
         player.setVelocityY(-500);
     }
+	
+	for (let star in stars.getChildren()) {
+		//star.lifespan -= Phaser.Core.TimeStep.delta;
+		star.lifespan --;
+		console.log(star.lifespan);
+		if (star.lifespan <= 0) {
+			star.destroy();
+		}
+	}
+	
+	// for (let bomb in bombs.getChildren) {
+		// console.log(bomb);
+		// if (bomb.anims.currentAnim) {
+			// if (!bomb.anims.isPlaying) {
+				// bomb.destroy();
+			// }
+		// }
+	// }
 }
 
 function collectStar (player, star)
 {
-    //star.disableBody(true, true);
+	star.emitter.remove();
     star.destroy();
     
     score += starCollectScore;
@@ -171,7 +203,7 @@ function collectStar (player, star)
             bomb.setVelocity(Phaser.Math.Between(-200, 200), 20);
             
             //add particle emitter to bomb
-            var emitter = particles.createEmitter({
+            bomb.emitter = bombParticles.createEmitter({
                 frequency: 60, 
                 radial: true,
                 lifespan: 500,
@@ -180,18 +212,21 @@ function collectStar (player, star)
                 angle: { random: true, start: 0, end: 270 },
                 //alpha: function (p) {return p.lifespan / emitter.lifespan;}
                 });
-            console.log(emitter);
-            emitter.follow = bomb;
+            bomb.emitter.follow = bomb;
         }
     }
 }
 
 function hitBomb (player, bomb)
 {
-    lives --;
-    livesText.setText(`LIVES: ${lives}`);
     if (lives > 1) {
-        bomb.destroy();
+		bomb.emitter.remove();
+		bomb.setVelocity(0, 0);
+		bomb.setGravity(0, 0);
+		bomb.anims.play('bombExplosion');
+		//bomb.destroy();
+		lives --;
+		livesText.setText(`LIVES: ${lives}`);
     } else {
         this.physics.pause();
         player.setTint(0xff0000);
@@ -225,5 +260,17 @@ function createStars () {
         star.setVelocity(Phaser.Math.Between(-200, 200), Phaser.Math.Between(20, 40));
         star.setCollideWorldBounds(true);
         star.setBounce(1);
+		star.lifespan = starLifespan;
+		
+		// particles follow the star
+		star.emitter = starParticles.createEmitter({
+                frequency: 50, 
+                radial: true,
+				alpha: 80,
+                lifespan: 100,
+                speed: 0
+                });
+            star.emitter.follow = star;
+		
     }
 }
